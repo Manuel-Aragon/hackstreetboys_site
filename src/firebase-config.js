499205f0f40e99682b8d04089c15dba4850fb1ad
Manuel Aragon <aragon.manuel@live.com>
2022-03-20 21:17:58 -0500

    removed dotenv module

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,8 +2,6 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-const dotenv = require('dotenv');
-dotenv.config();
 
 
 const firebaseConfig = {
