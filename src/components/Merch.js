ca0cbc340d1a1ae10e330e3f76859ad632e6be2b
Manuel Aragon <aragon.manuel@live.com>
2022-03-22 13:23:19 -0500

    added merch items

--- src/components/Merch.js
+++ src/components/Merch.js
@@ -7,7 +7,48 @@ export const Merch = ( ) => {
     return (
         <div>
             <Navbar/>
-            Shop
+            <div class="row row-cols-1 row-cols-md-3 mb-3 text-center bg-dark">
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                    <h4 class="my-0 fw-normal">Shirt 1</h4>
+                </div>
+                <div class="card-body">
+                    <button type="button" class="w-100 btn btn-lg btn-outline-primary">add to cart</button>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                    <h4 class="my-0 fw-normal">Shirt 2</h4>
+                </div>
+                <div class="card-body">
+                    <button type="button" class="w-100 btn btn-lg btn-outline-primary">add to cart</button>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                    <h4 class="my-0 fw-normal">Shirt 3</h4>
+                </div>
+                <div class="card-body">
+                    <button type="button" class="w-100 btn btn-lg btn-outline-primary">add to cart</button>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                    <h4 class="my-0 fw-normal">Shirt 3</h4>
+                </div>
+                <div class="card-body">
+                    <button type="button" class="w-100 btn btn-lg btn-outline-primary">add to cart</button>
+                </div>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
