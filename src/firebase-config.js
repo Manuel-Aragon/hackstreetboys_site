808bb90aa6c5e1fcc60513adffda168a85705a02
Manuel Aragon <aragon.manuel@live.com>
2022-04-13 16:02:18 -0500

    firebase changes

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,25 +1,25 @@
-
-   
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
 
-
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABSE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGE_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
-};
+    apiKey: "AIzaSyAyRTQ5Dp68MRN3_hW4c_L3Q05svz0T1nY",
+    authDomain: "hackstreetboys-1afc4.firebaseapp.com",
+    databaseURL: "https://hackstreetboys-1afc4-default-rtdb.firebaseio.com",
+    projectId: "hackstreetboys-1afc4",
+    storageBucket: "hackstreetboys-1afc4.appspot.com",
+    messagingSenderId: "746358041255",
+    appId: "1:746358041255:web:15a8ce7bfa1cb875b00982",
+    measurementId: "G-0E49N85SNN"
+  };
 
+  
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const fs = firebase.firestore();
 const storage = firebase.storage();
+
 export { auth, fs, storage }
