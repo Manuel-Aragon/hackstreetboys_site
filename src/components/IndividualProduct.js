eb54671cdaed51745c81543680d6034023fcd698
Manuel Aragon <aragon.manuel@live.com>
2022-04-20 14:32:02 -0500

    merch are now inside cards

--- src/components/IndividualProduct.js
+++ src/components/IndividualProduct.js
@@ -8,13 +8,19 @@ export const IndividualProduct = ({individualProduct, addToCart}) => {
     }   
     return (
         <div className='product'>
-            <div className='product-img'>
-                <img src={individualProduct.url} alt="product-img"/>
+            <div class="card mb-4 rounded-3 shadow-sm">
+                    <div class="card-header py-3">
+                        <div className='product-text title'>{individualProduct.title}</div>
+                    </div>
+                    <div class="card-body">
+                        <div className='product-img'>
+                            <img src={individualProduct.url} alt="product-img"/>
+                        </div>
+                        <div className='product-text description'>{individualProduct.description}</div>
+                        <div className='product-text price'>$ {individualProduct.price}</div>
+                        <div className='btn btn-danger btn-md cart-btn' onClick={handleAddToCart}>ADD TO CART</div>
+                    </div>
             </div>
-            <div className='product-text title'>{individualProduct.title}</div>
-            <div className='product-text description'>{individualProduct.description}</div>
-            <div className='product-text price'>$ {individualProduct.price}</div>
-            <div className='btn btn-danger btn-md cart-btn' onClick={handleAddToCart}>ADD TO CART</div>
         </div> 
     )
 }
