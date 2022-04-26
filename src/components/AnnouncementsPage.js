ba9838a5ec87290af2d55076dc8bc23eb6cfe54c
Manuel Aragon <aragon.manuel@live.com>
2022-04-25 22:17:07 -0500

    compile fix

--- src/components/AnnouncementsPage.js
+++ src/components/AnnouncementsPage.js
@@ -15,7 +15,7 @@ export const AnnouncementsPage = () => {
                             <div class="registration-right">
                                 <h2>Announcements</h2>
                                 <div class="event-list">
-                                    <div class="card flex-row"><img class="card-img-left img-fluid" src="" alt ="Announcement image" />
+                                <div class="card flex-row">
                                         <div class="card-body">
                                             <h4 class="card-title h5 h4-sm"><em class="fas fa-caret-right" aria-hidden="true"></em><span>APR 20 2022</span><em class="fas fa-caret-right" aria-hidden="true"></em><span>3:41 PM</span> </h4>
                                             <p class="card-text">The Hackstreet Boys are coming to Dallas, TX.</p>
@@ -23,32 +23,33 @@ export const AnnouncementsPage = () => {
 
                                         </div>
                                     </div>
-                                    <div class="card flex-row"><img class="card-img-left img-fluid" src="" alt = "Announcement image" />
+
+                                    <div class="card flex-row">
                                         <div class="card-body">
                                             <h4 class="card-title h5 h4-sm"><em class="fas fa-caret-right" aria-hidden="true"></em><span>APR 03 2022</span><em class="fas fa-caret-right" aria-hidden="true"></em><span>9:35 AM</span> </h4>
                                             <p class="card-text">Ahmed Alrimawi's wife gave birth to their 7th child this morning at 8:13 AM.</p>
-                                            <a href="url" target="_self">Read more</a>
+                                            <button id="modal-btn"> Read More</button>
                                         </div>
                                     </div>
-                                    <div class="card flex-row"><img class="card-img-left img-fluid" src="" alt = "Announcement image" />
+                                    <div class="card flex-row">
                                         <div class="card-body">
-                                            <h4 class="card-title h5 h4-sm"><em class="fas fa-caret-right" aria-hidden="true"></em><span>MAR 17 2022</span><em class="fas fa-caret-right" aria-hidden="true"></i><span>10:00 PM</span> </h4>
+                                            <h4 class="card-title h5 h4-sm"><em class="fas fa-caret-right" aria-hidden="true"></em><span>MAR 17 2022</span><em class="fas fa-caret-right" aria-hidden="true"></em><span>10:00 PM</span> </h4>
                                             <p class="card-text">We are very excited to announce that we will be in Greenville on April 23.</p>
-                                            <a href="url" target="_self">Read more</a>
+                                            <button id="modal-btn"> Read More</button>
                                         </div>
                                     </div>
-                                    <div class="card flex-row"><img class="card-img-left img-fluid" src="" alt = "Announcement image" />
+                                    <div class="card flex-row">
                                         <div class="card-body">
-                                            <h4 class="card-title h5 h4-sm"><em class="fas fa-caret-right" aria-hidden="true"></em><span>Feb 03 2022</span><em class="fas fa-caret-right" aria-hidden="true"></i><span>12:09 AM</span> </h4>
+                                            <h4 class="card-title h5 h4-sm"><em class="fas fa-caret-right" aria-hidden="true"></em><span>Feb 03 2022</span><em class="fas fa-caret-right" aria-hidden="true"></em><span>12:09 AM</span> </h4>
                                             <p class="card-text">Meet and Greet officially scheduled.</p>
-                                            <a href="url"target="_self">Read more</a>
+                                            <button id="modal-btn"> Read More</button>
                                         </div>
                                     </div>
-                                    <div class="card flex-row"><img class="card-img-left img-fluid" src="" alt = "Announcement image" />
+                                    <div class="card flex-row">
                                         <div class="card-body">
-                                            <h4 class="card-title h5 h4-sm"><em class="fas fa-caret-right" aria-hidden="true"></em><span>JAN 01 2022</span><em class="fas fa-caret-right" aria-hidden="true"></i><span>11:34 AM</span> </h4>
+                                            <h4 class="card-title h5 h4-sm"><em class="fas fa-caret-right" aria-hidden="true"></em><span>JAN 01 2022</span><em class="fas fa-caret-right" aria-hidden="true"></em><span>11:34 AM</span> </h4>
                                             <p class="card-text">The Hackstreet Boys are officially going on tour!</p>
-                                            <a href="url"target="_self">Read more</a>
+                                            <button id="modal-btn"> Read More</button>
                                         </div>
                                     </div>
                                 </div>
