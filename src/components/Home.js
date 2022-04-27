375dc980cfc1eb49830e932188f33bb30eba87e7
FCDallasJohnson <cadejohnson18@gmail.com>
2022-04-27 12:52:48 -0500

    Created Homepage
    Added the homepage with a link to the register page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,57 @@
 import React from 'react'
 import { Navbar } from './Navbar'
-import { Footer } from './Footer'
+import background from '../img/backgroundhome.png'
+import {useNavigate} from 'react-router-dom'
 
 export const Home = () => {
+    let navigate = useNavigate();
+
+    const register = async () => {
+        navigate('/Register');
+    };
 
     return (
         <div>
             <Navbar/>
             <div>
                 <br></br>
+                <div style={{ backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center',
+                textAlign: 'center', position: 'fixed', minWidth: '100%', minHeight: '100%', opacity: '0.9'}}>
+                    <div style={{position: 'absolute', top: 400, left: 0, right: 0, bottom: 0}}>
+                        <div class="px-4 py-5  text-center">
+                            <h1 class="display-1 fw-bold text-white">Hackstreet Boys on tour now!</h1>
+                            <div class="col-lg-6 mx-auto">
+                                <p class="lead mb-4 text-white">
+                                    Want tickets? Buy them today!
+                                </p>
+                                <li className='btn btn-secondary btn-md' onClick={register}>Register</li>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
             </div>
-            <Footer/>
         </div>
-        
     )
 }
+
+/*
+<div>
+            <Navbar/>
+            <div>
+                <br></br>
+                
+                <div class="px-4 py-5  text-center">
+                    <div style={{ backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center', height: '1000px'}}>
+                        <div style={{marginTop: '100px'}}>
+                            <h1 class="display-5 fw-bold text-white">Hackstreet Boys on tour now!</h1>
+                            <div class="col-lg-6 mx-auto">
+                                <p class="lead mb-4 text-white">
+                                Want tickets? Buy them today!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer/>
+        </div>*/
