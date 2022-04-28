a99926c275060f65b4615d3437fe62a02384cc0b
Manuel Aragon <aragon.manuel@live.com>
2022-04-27 19:22:21 -0500

    merch now shows front and back

--- src/components/Merch/IndividualProduct.js
+++ src/components/Merch/IndividualProduct.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import LogoShirt from '../../img/logo_shirt.jpg'
 
 export const IndividualProduct = ({individualProduct, addToCart}) => {
     // console.log(individualProduct);
@@ -11,9 +12,14 @@ export const IndividualProduct = ({individualProduct, addToCart}) => {
                     <div className='product-text title'>{individualProduct.title}</div>
                 </div>
                 <div class="card-body">
-                    <div className='product-img'>
-                        <img src={individualProduct.url} alt="product-img" width={150}/>
-                    </div>
+                        <div className='product-text description row row-cols-md-2'>
+                            <p>Front</p>
+                            <p>Back</p>
+                        </div>
+                        <div className='product-img'>
+                            <img src={LogoShirt} alt="product-img-front" width={150}/>
+                            <img src={individualProduct.url} alt="product-img-back" width={150}/>
+                        </div>
                     <div className='product-text description'>{individualProduct.description}</div>
                     <div className='product-text price'>$ {individualProduct.price}</div>
                     <div className='btn btn-danger btn-md cart-btn' onClick={handleAddToCart}>ADD TO CART</div>
